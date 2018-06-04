7b4c0900c485f6d8f7685fe27f36a30b04ab95cd
simon <simon.gavris@googlemail.com>
2018-06-04 02:29:43 +0200

    implementing Random Task Generator

--- src/main/java/com/simongavris/taskmanagement/controller/ApiController.java
+++ src/main/java/com/simongavris/taskmanagement/controller/ApiController.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
-public class TaskController {
+public class ApiController {
 
     @Autowired
     private TaskRepository taskRepository;
---
+++ src/main/java/com/simongavris/taskmanagement/util/ScheduledTask.java
@@ -0,0 +1,32 @@
+package com.simongavris.taskmanagement.util;
+
+import com.simongavris.taskmanagement.model.Task;
+import com.simongavris.taskmanagement.model.TaskQueue;
+import com.simongavris.taskmanagement.persistence.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTask {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTask.class);
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    @Autowired
+    private TaskRepository taskRepository;
+
+    //random.nextInt(max - min + 1) + min
+
+    @Scheduled(fixedDelayString = "${my.random.delay}")
+    public void createRandomTask() {
+        Task t = new Task("Random-Task-" + dateFormat.format(new Date()), Priority.MEDIUM, Status.OPEN, "This is a randomly created Task");
+        taskRepository.save(t);
+        TaskQueue.getInstance().add(t);
+        log.info("Creating Random Task at {}", dateFormat.format(new Date()));
+    }
+}
